818bca859acf84b454307d3d30c4c58263f48bc7
shchehlyuk-b-p <shbp87@gmail.com>
2021-11-29 10:48:34 +0200

    Enum

---
+++ src/Vehicles/Vehicles.java
@@ -0,0 +1,30 @@
+package Vehicles;
+
+public enum Vehicles {
+    BMW(1000, "black"), Mersedess (999, "white"), Toyota (800, "blue");
+
+    private int price;
+    public String colour;
+
+    Vehicles(int price, String colour) {
+        this.price = price;
+        this.colour = colour;
+    }
+
+    @Override
+    public String toString() {
+        return name()+"_Vehicles{" +
+                "price=" + price +
+                ", colour='" + colour + '\'' +
+                '}';
+    }
+}
+
+class Main {
+    public static void main (String [] arg) {
+        System.out.println(Vehicles.BMW.toString());
+        System.out.println(Vehicles.Mersedess.toString());
+        System.out.println(Vehicles.Toyota.toString());
+    }
+}
+
